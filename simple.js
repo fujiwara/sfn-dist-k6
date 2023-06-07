0abe39e8bfa03281d80672c9ada51269fca4b845
fujiwara <fujiwara.shunichiro@gmail.com>
2023-06-07 11:25:21 +0900

    add sleep

--- simple.js
+++ simple.js
@@ -1,8 +1,10 @@
 import http from 'k6/http'
 import { check } from 'k6'
+import { sleep } from 'k6'
 export default function () {
   let res = http.get('https://test.k6.io/news.php');
   check(res, { 'success': (r) => r.status === 200 });
+  sleep(1);
 }
 export function handleSummary(data) {
   return {
